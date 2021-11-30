9228c173c08e1f963a4e79f6d3d0621ec528728f
Anthony Kuang <AnthonyKuang116@gmail.com>
2021-11-30 12:54:07 -0500

    added redux

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,14 +1,42 @@
+// import React from 'react';
+
+// class Button extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {};
+//     console.log('Button Constructor');
+//   }
+//   render() {
+//     console.log('Button render');
+//     console.log(this.props);
+//     const { children, ...rest } = this.props;
+//     return <button {...rest}>{children}</button>;
+//   }
+//   componentDidMount() {
+//     console.log('Button componentDidMount');
+//   }
+//   componentWillUnmount() {
+//     console.log('Button componentWillUnmount');
+//   }
+// }
+
+// export default Button;
+
+
 import React from 'react';
+import { BuyStockContext } from '../context/BuyStock.context';
 
 class Button extends React.Component {
+  static contextType = BuyStockContext;
   constructor(props) {
     super(props);
     this.state = {};
-    console.log('Button Constructor');
+    // console.log('Button Constructor');
   }
   render() {
-    console.log('Button render');
-    console.log(this.props);
+    // console.log('Button render');
+    // console.log(this.props);
+    console.log('Button Context', this.context);
     const { children, ...rest } = this.props;
     return <button {...rest}>{children}</button>;
   }
@@ -17,6 +45,7 @@ class Button extends React.Component {
   }
   componentWillUnmount() {
     console.log('Button componentWillUnmount');
+    this.unsubscribe();
   }
 }
 
--- src/components/Counter/CounterFn.js
+++ src/components/Counter/CounterFn.js
@@ -1,56 +1,223 @@
+// import React from 'react';
+// import Button from '../Button/Button';
+
+// let data = {
+//     counter: 1000,
+// };
+// // UI = React (state)
+
+// function CounterFn({ count, setCount, hideBtn, setHideBtn }) {
+//     const addCountHandler = () => {
+//         setCount(count + 1);
+//     }
+
+//     const countAlertHandler = () => {
+//         setTimeout(() => alert(count), 5000);
+//     }
+
+//     const showBtnHandler = () => {
+//         if(hideBtn === true){
+//             setHideBtn(false);
+//         }
+//         if(hideBtn === false){
+//             setHideBtn(true)
+//         }
+//     }
+
+//     return (
+//         <section>
+//             <header>Counter:{count}</header>
+
+//             {hideBtn ? (
+//                 <Button
+//                     className="btn"
+//                     onClick={addCountHandler}
+//                 >
+//                     <h1>ADD</h1>
+//                 </Button>
+//             ) : null}
+
+//             <Button onClick={showBtnHandler}>Remove/Show Btn Add</Button>
+//             <Button onClick={countAlertHandler}>Alert After 5s</Button>
+//         </section>
+//     );
+// }
+
+
+
+// /*
+// Countr Explaination with setState(HW1):
+
+// I believe the reason why even though we had 3 lines of updates to increase count by one, it will
+// only update once the component is unmounted and rendered again.
+// */
+
+
+// export default CounterFn;
+
+
+
+
 import React from 'react';
 import Button from '../Button/Button';
+import { mystore } from '../../MyRedux/MyRedux';
+import { useForceUpdate } from '../../hooks/useForceUpdate';
 
 let data = {
-    counter: 1000,
+  counter: 1000,
 };
 // UI = React (state)
 
-function CounterFn({ count, setCount, hideBtn, setHideBtn }) {
-    const addCountHandler = () => {
-        setCount(count + 1);
-    }
+class Counter extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: 'counter',
+      hideBtnAdd: false,
+    };
+    //this.forceUpdateTest = this.forceUpdateTest.bind(this);
 
-    const countAlertHandler = () => {
-        setTimeout(() => alert(count), 5000);
-    }
+    console.log('Counter constructor');
+  }
 
-    const showBtnHandler = () => {
-        if(hideBtn === true){
-            setHideBtn(false);
-        }
-        if(hideBtn === false){
-            setHideBtn(true)
-        }
+  static getDerivedStateFromProps(props, state) {
+    console.log('Counter getDerivedStateFromProps');
+    if (props.title !== state.title) {
+      //Change in props
+      return {
+        title: props.title,
+      };
     }
+    return null; // No change to state
+  }
+  render() {
+    console.log('Counter render');
 
     return (
-        <section>
-            <header>Counter:{count}</header>
+      <section>
+        {this.props.children}
+        <header>
+          {this.state.title}:{mystore.getState().value}
+        </header>
 
-            {hideBtn ? (
-                <Button
-                    className="btn"
-                    onClick={addCountHandler}
-                >
-                    <h1>ADD</h1>
-                </Button>
-            ) : null}
+        {!this.state.hideBtnAdd ? (
+          <Button
+            className="btn"
+            onClick={() => {
+              //HW1  why
+              mystore.dispatch({ type: 'counter/incremented' });
+            }}
+          >
+            Add
+          </Button>
+        ) : null}
+        <Button
+          className="btn"
+          onClick={() => {
+            mystore.dispatch({ type: 'counter/decremented' });
+          }}
+        >
+          Sub
+        </Button>
 
-            <Button onClick={showBtnHandler}>Remove/Show Btn Add</Button>
-            <Button onClick={countAlertHandler}>Alert After 5s</Button>
-        </section>
+        <Button
+          onClick={() => {
+            this.setState({ hideBtnAdd: !this.state.hideBtnAdd });
+          }}
+        >
+          Remove/Show Btn Add
+        </Button>
+        <Button
+          onClick={() => {
+            let currentCouter = this.state.counter;
+            setTimeout(() => {
+              alert(currentCouter); // HW2 alert the recent value using function component
+            }, 5000);
+          }}
+        >
+          Alert After 5s
+        </Button>
+      </section>
     );
+  }
+
+  componentDidMount() {
+    mystore.subscribe(() => this.forceUpdate());
+  }
 }
 
+let alertCounter;
+export const CounterFn = (props) => {
+  const forceUpdate = useForceUpdate();
+  const [hideBtnAdd, setHideBtnAdd] = React.useState(false);
+  const [showAlert, setShowAlert] = React.useState(false);
 
+  React.useEffect(() => {
+    if (showAlert === true) {
+      alert('counter');
+      setShowAlert(false);
+    }
+  }, [showAlert]);
 
-/*
-Countr Explaination with setState(HW1):
+  // ComponentDidMount
+  // React.useEffect(() => {
+  //   console.log('useEffect invoked');
+  // }, []);
 
-I believe the reason why even though we had 3 lines of updates to increase count by one, it will
-only update once the component is unmounted and rendered again.
-*/
+  // ComponentDidUpdate + ComponentDidMount
+  // React.useEffect(() => {
+  //   console.log('useEffect invoked');
+  // });
 
+  // React.useEffect(() => {
+  //   return () => {
+  //     // ComponentWillUnmount
+  //   };
+  // }, [XXX]);
 
-export default CounterFn;
+  React.useEffect(() => {
+    mystore.subscribe(() => {
+      forceUpdate();
+    });
+  }, []);
+
+  return (
+    <section>
+      {props.children}
+      <header>
+        {props.title}:{mystore.getState().value}
+      </header>
+
+      {!hideBtnAdd ? (
+        <Button
+          className="btn"
+          onClick={() => {
+            mystore.dispatch({ type: 'counter/incremented' });
+            console.log(mystore.getState());
+          }}
+        >
+          <h1>ADD</h1>
+        </Button>
+      ) : null}
+
+      <Button
+        onClick={() => {
+          setHideBtnAdd(!hideBtnAdd);
+        }}
+      >
+        Remove/Show Btn Add
+      </Button>
+      <Button
+        onClick={() => {
+          setTimeout(() => {
+            setShowAlert(true); // HW2 alert the recent value using function component
+          }, 5000);
+        }}
+      >
+        Alert After 5s
+      </Button>
+    </section>
+  );
+};
+
+export default Counter;
